8db6acddc7f27713a819e4bd5de009676869950f
Benson Varghese <bvarghese@uwm.com>
2022-11-26 21:57:00 -0500

    Add project files.

---
+++ src/CommandAPI/CommandAPI/Controllers/CommandsController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace CommandAPI.Controllers
+{
+    [Route("api/commands")]
+    [ApiController]
+    public class CommandsController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            return new string[] { "this", "is", "hard", "coded" };
+        }
+    }
+}
